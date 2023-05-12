4af27b8af0eac500be369097ba962f83a4d30452
Safwan Ull Karim <imsafwanuk@users.noreply.github.com>
2023-05-13 08:55:00 +1000

    Update SignIn-Redirect.js

--- PowerPages/SignIn-Redirect.js
+++ PowerPages/SignIn-Redirect.js
@@ -1,7 +1,7 @@
 // this redirect is only enabled for B2C signins and signups.
 debugger;
 if (document.referrer.indexOf("b2clogin.com") > -1) {
-  // take return query here i.e. "https://myportal/profile?/mypage?id=32fe-325fgw-sr32" and add create url from it, i.e. "https://myportal/mypage?id=32fe-325fgw-sr32"
+  // take return query here i.e. "https://myportal/?ReturnUrl=/mypage?id=32fe-325fgw-sr32" and add create url from it, i.e. "https://myportal/mypage?id=32fe-325fgw-sr32"
   const searchParams = new URLSearchParams(window.location.href.split("?")[1]);
   const paramVal = searchParams.get("ReturnUrl");
   if (paramVal) {
